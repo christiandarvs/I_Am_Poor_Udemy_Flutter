b2d84d5e00b1e5b27eb8ba1211bfeebd4815865c
christiandarvs <christiandarvin4@gmail.com>
2023-01-26 17:09:16 +0800

    add files, images and edit main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: const Color(0xff607D8B),
+          backgroundColor: const Color(0xff546E7A),
           centerTitle: true,
           title: const Text('I Am Poor'),
         ),
-        body: Center(child: Image.asset('name')),
+        backgroundColor: const Color(0xffF5F5F5),
+        body: Center(child: Image.asset('images/money-loss.png')),
       ),
     );
   }
